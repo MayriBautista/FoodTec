4f0796c5cead94ecb0ffe54b57a87ad95c6c18af
Jose Manuel <josemanuelgarciadelao@gmail.com>
2019-06-04 10:30:32 -0500

    cambiesillos

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -10,19 +10,11 @@ export class HttpService {
   constructor(public http:HttpClient) { }
 
   login(usuario:string, contra:string){
-    let datos = { usuario:usuario, contra:contra}
-  
-    let options = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
- 
-    //alert(usuario+contra);
+   
    
    var url = 'http://avisositd.xyz/mobiliaria/loginMobiliaira.php/'
    return new Promise((resolve, reject) => {
-    this.http.post(url,JSON.stringify(datos),options)
+    this.http.get(url)
        .subscribe(data => {
          resolve(data);
         }, (err) =>{
@@ -34,7 +26,7 @@ export class HttpService {
   registro(usuario:string, contra:string, correo:string, telefono:string){
     
    
-   var url = 'http://avisositd.xyz/mobiliaria/loginMobiliaira.php?usuario='+usuario+'&contra='+contra;
+   var url = 'http://avisositd.xyz/mobiliaria/login/'+usuario+'/'+contra;
    return new Promise((resolve, reject) => {
     this.http.get(url)
        .subscribe(data => {
