7157b7a3d6f93444a2d3d51303965ea887f52389
Mayri Bautista <16041328@itdurango.edu.mx>
2019-06-05 18:47:14 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'billetera', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'cuenta', loadChildren: './cuenta/cuenta.module#CuentaPageModule' },
@@ -10,6 +10,8 @@ const routes: Routes = [
   { path: 'cambiarcontra', loadChildren: './cambiarcontra/cambiarcontra.module#CambiarcontraPageModule' },
   { path: 'cambiarnum', loadChildren: './cambiarnum/cambiarnum.module#CambiarnumPageModule' },
   { path: 'eliminar', loadChildren: './eliminar/eliminar.module#EliminarPageModule' },
+  { path: 'billetera', loadChildren: './billetera/billetera.module#BilleteraPageModule' },
+
 
 
 
---
+++ src/app/billetera/billetera.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { MenuController, Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-billetera',
+  templateUrl: './billetera.page.html',
+  styleUrls: ['./billetera.page.scss'],
+})
+export class BilleteraPage {
+  rootPage:any = 'TablePage';
+
+  constructor(private menu: MenuController) { 
+  }
+
+  openFirst() {
+    console.log("click OpenFirst");
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    console.log("click OpenEnd");
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    console.log("click OpenCustom");
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+}
