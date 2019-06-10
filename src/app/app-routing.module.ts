2b6a657f3104c72f9f976d8ae353e7f49d5db34e
Mayri Bautista <16041328@itdurango.edu.mx>
2019-06-10 13:47:04 -0500

    compra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'billetera', pathMatch: 'full' },
+  { path: '', redirectTo: 'cuenta', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'cuenta', loadChildren: './cuenta/cuenta.module#CuentaPageModule' },
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: 'cambiarcontra', loadChildren: './cambiarcontra/cambiarcontra.module#CambiarcontraPageModule' },
   { path: 'cambiarnum', loadChildren: './cambiarnum/cambiarnum.module#CambiarnumPageModule' },
   { path: 'eliminar', loadChildren: './eliminar/eliminar.module#EliminarPageModule' },
-  { path: 'billetera', loadChildren: './billetera/billetera.module#BilleteraPageModule' },
+  { path: 'billetera', loadChildren: './billetera/billetera.module#BilleteraPageModule' },  { path: 'compra', loadChildren: './compra/compra.module#CompraPageModule' },
+
 
 
 
